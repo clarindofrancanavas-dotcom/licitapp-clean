0cca0b1db76f4371b29a4a410f8f0b7f3d22b0e7
clarindofrancanavas-dotcom <clarindofrancanavas@gmail.com>
2026-03-19 00:21:25 -0300

    corrige datas pncp

--- app/api/pncp/route.js
+++ app/api/pncp/route.js
@@ -1,9 +1,9 @@
 export async function GET() {
   try {
-    const res = await fetch(
-      "https://pncp.gov.br/api/consulta/v1/contratacoes/publicacao?pagina=1&tam_pagina=5"
-    );
+    const url =
+      "https://pncp.gov.br/api/consulta/v1/contratacoes/publicacao?pagina=1&tam_pagina=5&dataInicial=20250101&dataFinal=20261231";
 
+    const res = await fetch(url);
     const json = await res.json();
 
     return Response.json(json);
