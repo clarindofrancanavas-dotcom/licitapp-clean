93e2791a13dfb62ebc91757a4040614f40055bc4
clarindofrancanavas-dotcom <clarindofrancanavas@gmail.com>
2026-03-19 01:38:10 -0300

    Update route.js

--- app/api/importar/route.js
+++ app/api/importar/route.js
@@ -8,7 +8,7 @@ export async function GET() {
     );
 
     const url =
-      "https://pncp.gov.br/api/consulta/v1/contratacoes/proposta?dataFinal=20261231&codigoModalidadeContratacao=8&pagina=1&tamanhoPagina=2";
+      "https://pncp.gov.br/api/consulta/v1/contratacoes/proposta?dataFinal=20261231&codigoModalidadeContratacao=8&pagina=2&tamanhoPagina=10";
 
     const res = await fetch(url);
     const json = await res.json();
